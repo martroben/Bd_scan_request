25a4b0aa2f85a0f0ccea41a06dbcc99b415f0992
martroben <87522742+martroben@users.noreply.github.com>
2022-08-18 22:49:11 +0300

    Update Bd_scan_request.sh
    Added standard script information

--- Bd_scan_request.sh
+++ Bd_scan_request.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+########################################################################################################################################
+##                                                                                                                                    ##
+##  Script name: Bd_scan_request.R                                                                                                    ##
+##  Purpose of script: Prompt Linux users to start a Bitdefender scan. Meant to be run as a hourly cron job.                          ##
+##                     Frequency of promopts increases as time since last scan increases.                                             ##
+##                                                                                                                                    ##
+##  Dependencies:                                                                                                                     ##
+##    bash calculator (bc -v to check)                                                                                                ##
+##    zenity (zenity --version to check)                                                                                              ##
+##                                                                                                                                    ##
+##  Author: Mart Roben                                                                                                                ##
+##  Date Created: 18. Aug 2022                                                                                                        ##
+##  Contact: mart@altacom.eu                                                                                                          ##
+##                                                                                                                                    ##
+##  Copyright: MIT License                                                                                                            ##
+##  https://github.com/martroben/Bd_scan_Request                                                                                      ##
+##                                                                                                                                    ##
+##  Contact: mart@altacom.eu                                                                                                          ##
+##                                                                                                                                    ##
+########################################################################################################################################
 
-# Dependencies:
-# bash calculator (bc -v to check)
-# zenity (zenity --version to check)
 
 Bd_install_location="/opt/bitdefender-security-tools"
 Bd_full_scan_identifier="dcf483c4-26d0-4e6f-ba28-6a53a00adae1"	# Bitdefender says it's the same for all agents and devices
 Bd_full_scan_logs="$Bd_install_location/var/log/$Bd_full_scan_identifier"
 
 # Get number of days since last full scan
-# If would be nicer to use bduitool to get the last scan time, but it produced buggy results in some tests
+# If would be nicer to use bduitool to get the last scan time, but it produced buggy results in some of my test runs
 current_time=$(date +%s)
 last_full_scan_time=$(sudo find $Bd_full_scan_logs/*.xml -printf "%T@\n" | sort -n | tail -n 1)
 if [[ $last_full_scan_time ]]; then
@@ -30,7 +47,6 @@ prompt_agree_option="Jah"
 prompt_reject_option="Ei"
 prompt_timeout=$(echo "3600 * 24 / (23 / $frequency_parameter + 1)" | bc)
 
-
 # Check that there are no full scans already running
 # Check if it's the correct hour of the day to display the dialogue
 # Ask user to start a full scan
@@ -48,10 +64,9 @@ if [[ $(($current_hour % $frequency_parameter)) -eq 0 && -z $active_scan ]]; the
       --text="$prompt_message" \
       --ok-label "$prompt_agree_option" \
       --cancel-label "$prompt_reject_option"
-
-    prompt_answer=$?
-
+    
     # If user agrees, start a full scan and display an info prompt
+    prompt_answer=$?
     if [[ $prompt_answer -eq 0 ]]; then
         sudo $Bd_install_location/bin/bduitool scan -s full
         zenity --info \
